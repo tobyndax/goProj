454c3db02dd083c2a6972af112824649d457121a
Jens Edhammer <tobyndax@gmail.com>
2018-07-11 22:19:42 +0200

    printWithDelay implemented. The function writes text to the terminal with a random delay per character.

--- hello.go
+++ hello.go
@@ -1,6 +1,18 @@
 package main
 import "fmt"
+import "math/rand"
+import "time"
+
+func printRandDelay(s string, maxDelayMs float64){
+	for _, char := range s {
+		fmt.Printf("%c",char)
+		localDuration := time.Duration(maxDelayMs*rand.Float64())
+		time.Sleep(localDuration*time.Millisecond)
+	}
+	fmt.Println("")
+	return
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	printRandDelay("Hello World!",200)
 }
