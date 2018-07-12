d7c72e72c6422486d8f37ecddb6b47025c47cb6a
Jens Edhammer <tobyndax@gmail.com>
2018-07-12 10:15:16 +0200

    Moved over functions from main.go to display.go

--- main.go
+++ main.go
@@ -1,20 +1,7 @@
 package main
-import "fmt"
-import "math/rand"
-import "time"
 import "display"
 
-func printRandDelay(s string, maxDelayMs float64){
-	for _, char := range s {
-		fmt.Printf("%c",char)
-		localDuration := time.Duration(maxDelayMs*rand.Float64())
-		time.Sleep(localDuration*time.Millisecond)
-	}
-	fmt.Println("")
-	return
-}
 
 func main() {
-	display.NewFunc()
-	printRandDelay("Hello World!",200)
+	display.PrintRandDelay("Hello World!",200)
 }
--- src/display/display.go
+++ src/display/display.go
@@ -1,7 +1,15 @@
 package display
 
+import "math/rand"
+import "time"
 import "fmt"
 
-func NewFunc(){
-	fmt.Println("This is a new function")
+func PrintRandDelay(s string, maxDelayMs float64){
+	for _, char := range s {
+		fmt.Printf("%c",char)
+		localDuration := time.Duration(maxDelayMs*rand.Float64())
+		time.Sleep(localDuration*time.Millisecond)
+	}
+	fmt.Println("")
+	return
 }
